1df8a3b498d93d6e613173b72a2591ad1899c0f7
Juan23X <159858764+Juan23X@users.noreply.github.com>
2024-02-20 17:26:34 -0500

    Add files via upload

---
+++ JScript/jscript/class3.js
@@ -0,0 +1,64 @@
+let PI = 3.1416;
+console.log(PI);
+let e = 2.71325;
+
+let name = "Pablo";
+console.log(name);
+
+name = "Juan";
+console.log(name);
+
+let suma = PI+e;
+console.log(suma);
+
+PI === name? console.log("verdadero"):console.log("falso");
+
+
+const VidasIniciales = 30;
+
+
+
+/*Declaracion de variables sin asignar un valor*/
+
+let playerName;
+
+const euler = 1.0;
+
+let empty = null;
+
+
+/* Métodos */
+
+Math.exp(3);
+let nombreUsuario = "Pedro";
+console.log(nombreUsuario.length);
+console.log(nombreUsuario.includes("P"));
+
+
+//Métodos o Funciones//
+
+//recibe parámetros dos//
+
+const sumaValores = (numberOne, numberTwo) =>{
+    return numberOne+numberTwo
+}
+
+const imprimirNombre = () =>{
+    console.log("Juan");
+}
+
+const saludar = nombreAImprimir => {
+    return "Hello" + nombreAImprimir;
+}
+
+console.log(saludar(" Pedro"));
+
+
+// crear 3 arrow function 
+// 1. recibe cero parametros
+// 2. recibe 1 parametro
+// 3. recibe mas de dos parametros
+
+//CONST 3
+
+//LET 5
